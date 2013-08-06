83a6ee79d1eefd1384a7b021320b0d1ef7cf1152
supersymmetry <maildumpert@gmail.com>
2013-08-06 23:01:07 +0200

    update to 1.5.1

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "A Clojure wrapper around the Lanterna terminal output library."
   :url "http://sjl.bitbucket.org/clojure-lanterna/"
   :license {:name "LGPL"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.googlecode.lanterna/lanterna "2.1.5"]]
   :java-source-paths ["./java"]
   ; :repositories {"sonatype-snapshots" "https://oss.sonatype.org/content/repositories/snapshots"}
